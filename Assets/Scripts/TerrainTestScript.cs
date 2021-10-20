6ef40c6936aa46aaa1ae3aa4d8cc7c972f70c73a
Daniel Clipston <daniel_clipston@hotmail.com>
2021-10-20 12:42:33 +0100

    terrain Testing and modification
    A script which tested how to make terrain in code - but not manipulate them (which is next)
    Changes to prefabs to give them a rigidbody with kinematic control

---
+++ Assets/Scripts/TerrainTestScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainTestScript : MonoBehaviour
+{
+    [SerializeField]
+    GameObject prefabObject;//!<GameObject prefab containing the terrain
+    Terrain prefabTerrain;//!<The terrain component of the prefab
+    Terrain currentTerrain;//!<The current terrain component
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Default terrain size is 1000x1000
+        currentTerrain = gameObject.GetComponent<Terrain>();
+        Instantiate(prefabObject, new Vector3(-1000,0,0), Quaternion.identity);//!<Create the object holding the terrain
+        prefabTerrain = prefabObject.GetComponent<Terrain>();//!<Get the terrain component
+        currentTerrain.SetNeighbors(prefabTerrain, null, null, null);//!<Set the neighbours (Neighbour in a purely logical sense, possibly used to texture mapping and rendering)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
